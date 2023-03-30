7fe44ccabf33cf5ac73673e435946354e699a7c5
Lucas <contatolucasm9@gmail.com>
2023-03-30 09:20:26 -0300

    psn clone done

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  @Input()
+  gameLabel:string=""
+  @Input()
+  gameCover:string=""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
